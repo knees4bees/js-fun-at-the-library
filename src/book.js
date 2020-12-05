f264051e51755765246f961d56a7f5bc5c930a5a
Katie Ballentine <knees4bees@gmail.com>
2020-12-05 10:42:30 -0700

    Write and refactor functions

--- src/book.js
+++ src/book.js
@@ -1,12 +1,56 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+
+  return character;
+}
+
+
+function saveReview(review, reviews) {
+  if (reviews.includes(review) === false) {
+    reviews = reviews.push(review);
+  }
+}
+
+
+function calculatePageCount(title) {
+  var pageCount = title.length * 20
+
+  return pageCount;
+}
+
+
+function writeBook(title, character, genre) {
+  var book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  }
+
+  return book;
+}
+
+
+function editBook(book) {
+  book.pageCount = (3/4) * book.pageCount;
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
