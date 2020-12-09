45776fa11f712b17217ac0b7063aac21531f9a0b
Katie Ballentine <knees4bees@gmail.com>
2020-12-08 23:45:30 -0700

    Fix spacing and indentation in librarian.js per feedback from Kayla

--- src/librarian.js
+++ src/librarian.js
@@ -6,44 +6,40 @@ class Librarian {
     this.library = library;
   }
 
-
-greetPatron(name, isMorning) {
-  if (isMorning === true) {
-    return `Good morning, ${name}!`;
-  } else {
-    return `Hello, ${name}!`;
-  }
-}
-
-
-findBook(titleString) {
-
-  var genreList = Object.keys(this.library.shelves);
-  var isOnShelf;
-  var message = "";
-
-  for (var i = 0; i < genreList.length; i++) {
-    for (var j = 0; j < this.library.shelves[genreList[i]].length; j++) {
-      if (this.library.shelves[genreList[i]][j].title === titleString) {
-        isOnShelf = true;
-        lib.checkoutBook(this.library, titleString, genreList[i]);
-        message = `Yes, we have ${titleString}`;
-      } else {
-        isOnShelf = false;
-        message = `Sorry, we do not have ${titleString}`;
-      }
+  greetPatron(name, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
     }
   }
 
-  return message;
-}
+  findBook(titleString) {
+    var genreList = Object.keys(this.library.shelves);
+    var isOnShelf;
+    var message = "";
 
+    for (var i = 0; i < genreList.length; i++) {
+      for (var j = 0; j < this.library.shelves[genreList[i]].length; j++) {
+        if (this.library.shelves[genreList[i]][j].title === titleString) {
+          isOnShelf = true;
+          lib.checkoutBook(this.library, titleString, genreList[i]);
+          message = `Yes, we have ${titleString}`;
+        } else {
+          isOnShelf = false;
+          message = `Sorry, we do not have ${titleString}`;
+        }
+      }
+    }
 
-calculateLateFee(daysLate) {
-  var lateFee = Math.ceil(0.25 * daysLate);
+    return message;
+  }
 
-  return lateFee;
-}
+  calculateLateFee(daysLate) {
+    var lateFee = Math.ceil(0.25 * daysLate);
+
+    return lateFee;
+  }
 
 }
 
